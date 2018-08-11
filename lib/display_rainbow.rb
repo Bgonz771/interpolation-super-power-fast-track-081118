1cb95dc904f6a9cb00b956ead7dd4f01f620fc49
billy@arkhamgc.com <billy@arkhamgc.com>
2018-08-11 17:20:07 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 
 def #display_rainbow(color_array)
-puts "[ red[0],orange[1],yellow[2],green[3],blue[4],indigo[5],violet[6]"
+puts r #"[ red[0],orange[1],yellow[2],green[3],blue[4],indigo[5],violet[6]"
 end
   
